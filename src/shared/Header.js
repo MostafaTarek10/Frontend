94acd890da4f530d7089edfe498456d0687f6ed1
mostafa <mostafaboda305@gmail.com>
2023-04-20 19:53:50 +0200

    medicine-manage added with some changes

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,27 +1,46 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import "../Style/Header.css"
+import "../Style/Header.css";
 
 const Header = () => {
-  return <>
-    <Navbar bg="dark" variant="dark">
+  let Logout=()=>{
+    <div>
+      Logout
+    </div>
+  }
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand >
-          <Link to={"/"} className="nav-link">MedsCat</Link>
+          <Navbar.Brand>
+            <Link to={"/"} className="nav-link">
+              Medicine categorize
+            </Link>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Link to={"/ManageMedicines"} className="nav-link">Medicines</Link>
-            <Link to={"/login"} className="nav-link">Login</Link>
-            <Link to={"/register"} className="nav-link">Register</Link>
-
+            <Link to={"/"} className="nav-link">
+              Medicine list
+            </Link>
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+            <Link to={"/register"} className="nav-link">
+              Register
+            </Link>
+            <Link to={"/manage-medicine"} className="nav-link">
+              Manage Medicine
+            </Link>
           </Nav>
-          <Link to={"/test"} className="sign-in">Sign in</Link>
+          <Nav className="logout">
+            <Nav.Link onClick={Logout}>Logout</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-  </>;
+    </>
+  );
 };
 
 export default Header;
