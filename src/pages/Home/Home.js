5704fa0ccdfd7ff9556016db8ce29cf6462e3325
Omarashraf2002 <Omarash2020@gmail.com>
2023-04-21 22:10:00 +0200

    Updated Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MedCat from "../../components/MedCat";
+import MedCat from "../../Components/MedCat";
 import "../Home/Home.css";
 import Form from "react-bootstrap/Form";
 
